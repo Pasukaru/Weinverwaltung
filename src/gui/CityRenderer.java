eff1b99fe4084902d64f91d992b510b157f00ad0
Pascal Ludwig <pascal.ludwig@mantro.net>
2014-02-10 09:38:31 +0100

    asdf

--- src/gui/CityRenderer.java
+++ src/gui/CityRenderer.java
@@ -1,32 +1,13 @@
 package gui;
 
-import java.awt.Component;
-
-import javax.swing.JList;
-import javax.swing.plaf.basic.BasicComboBoxRenderer;
-
 import model.City;
 
-public class CityRenderer extends BasicComboBoxRenderer {
+public class CityRenderer extends ModelRenderer<City> {
 
 	private static final long serialVersionUID = 6745412362568365583L;
 
 	@Override
-	public Component getListCellRendererComponent(
-		@SuppressWarnings("rawtypes") JList list, Object value, int index,
-		boolean isSelected, boolean cellHasFocus) {
-		super.getListCellRendererComponent(
-			list,
-			value,
-			index,
-			isSelected,
-			cellHasFocus);
-
-		if (value != null) {
-			City city = (City) value;
-			setText(city.getName());
-		}
-
-		return this;
+	public String getValue(City city) {
+		return city.getName();
 	}
 }
--- src/gui/TypeRenderer.java
+++ src/gui/TypeRenderer.java
@@ -1,32 +1,13 @@
 package gui;
 
-import java.awt.Component;
-
-import javax.swing.JList;
-import javax.swing.plaf.basic.BasicComboBoxRenderer;
-
 import model.Type;
 
-public class TypeRenderer extends BasicComboBoxRenderer {
+public class TypeRenderer extends ModelRenderer<Type> {
 
 	private static final long serialVersionUID = 6745412362568365583L;
 
 	@Override
-	public Component getListCellRendererComponent(
-		@SuppressWarnings("rawtypes") JList list, Object value, int index,
-		boolean isSelected, boolean cellHasFocus) {
-		super.getListCellRendererComponent(
-			list,
-			value,
-			index,
-			isSelected,
-			cellHasFocus);
-
-		if (value != null) {
-			Type type = (Type) value;
-			setText(type.getName());
-		}
-
-		return this;
+	public String getValue(Type type) {
+		return type.getName();
 	}
 }
