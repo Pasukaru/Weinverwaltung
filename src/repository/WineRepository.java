5a1268123ac11201d6993bad472128cd5ecf4486
Pascal Ludwig <pascal.ludwig@mantro.net>
2014-02-24 15:14:20 +0100

    improved WineRepository.search

--- src/repository/WineRepository.java
+++ src/repository/WineRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -24,24 +25,34 @@ public class WineRepository extends Repository<Wine>{
 		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Wine> cq = cb.createQuery(model);
         Root<Wine> root = cq.from(Wine.class);
-		
+
         List<Predicate> conditions = new ArrayList<Predicate>();
-		
-		String[] joins = {"winery", "type", "sort", "city", "vine"};
-		
-		for(String join : joins){
-			Expression<String> path = root.join(join).get("name");
+
+		Expression<String> path = root.get("name");
+		conditions.add(cb.like(path, query));
+
+        List<Join<Object, Object>> joins = new ArrayList<Join<Object, Object>>();
+
+		Join<Object, Object> city = root.join("city");
+		Join<Object, Object> region = city.join("region");
+		Join<Object, Object> country = region.join("country");
+
+		joins.add(city);
+		joins.add(region);
+		joins.add(country);
+		joins.add(root.join("winery"));
+		joins.add(root.join("type"));
+		joins.add(root.join("sort"));
+		joins.add(root.join("vine"));
+
+		for(Join<Object, Object> join : joins){
+			path = join.get("name");
 			conditions.add(cb.like(path, query));
 		}
 
-		Expression<String> regionPath = root.join("city").join("region").get("name");
-		conditions.add(cb.like(regionPath, query));
-		Expression<String> countryPath = root.join("city").join("region").join("country").get("name");
-		conditions.add(cb.like(countryPath, query));
-		
 		cq.distinct(true);
 		cq.where(cb.or(conditions.toArray(new Predicate[conditions.size()])));
-		
+
 		return entityManager.createQuery(cq).getResultList();
 	}
 }
